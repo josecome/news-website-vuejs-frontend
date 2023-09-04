d89be2b6a3bfb7b4d9d7f4dee6b548a2a28242e2
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-04 18:41:03 +0200

    routes for news and dashboard, code in router to manage user auth status added

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,19 @@ const router = createRouter({
       component: Home
     },
     {
+      path: '/news/:id',
+      name: 'news',
+      component: Products_for_sales
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -19,5 +32,21 @@ const router = createRouter({
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(page => page.meta.requiresAuth)) { //to.name !== 'login' && !this.isloggedin
+    const auth_store = useStoreAuth()
+    const {  isLoggedin } = storeToRefs(auth_store)
+    if ( !isLoggedin.value ) { //Is not logged in, go to Login Page
+      next({ 
+        name: 'login' 
+        //path: 'login',
+        //replace: true
+      })
+    } else {
+      next() // go to wherever I'm going
+    }
+  } else {
+    next() // does not require auth, make sure to always call next()!
+  }
+})
 export default router
